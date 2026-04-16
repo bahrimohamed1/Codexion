79b103912d3a58099774eab615cbc3307f2f57d5
Mohamed Bahri <mbahri@c3r5p4.1337.ma>
2026-04-16 20:08:53 +0100

    parsing beta

--- codexion.c
+++ codexion.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbahri <mbahri@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/16 14:13:29 by mbahri            #+#    #+#             */
-/*   Updated: 2026/04/16 14:48:26 by mbahri           ###   ########.fr       */
+/*   Created: 2026/04/16 14:13:29 by username          #+#    #+#             */
+/*   Updated: 2026/04/16 20:08:05 by mbahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	is_digit(char *s)
 	int	i;
 
 	i = 0;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i++] == '-')
+			return (0);
+	}
 	while (s[i])
 	{
 		if (!(s[i] >= '0' && s[i] <= '9'))
@@ -26,36 +31,49 @@ int	is_digit(char *s)
 	return (1);
 }
 
-int parse_arguments(int argc, char **argv, t_simulation *sim)
+int	parse_arguments(char **argv, t_simulation *sim)
 {
-	if (argc == 9)
+	int	i;
+
+	i = 1;
+	while (i < 8)
 	{
-		int	i;
-
-		i = 0;
-		while	(argv[i])
-		{
-			if (!is_digit(argv[i]))
-				return (1);
-
-			i++;
-		}
-			return 1;
-		int number_coders = atoi(argv[1]);
-		int	burnout_time = atoi(argv[2]);
-		int compile_time = atoi(argv[3]);
-		int debug_time = atoi(argv[4]);
-		int refactor_time = atoi(argv[5]);
-		int number_compiles = atoi(argv[6]);
-		int dongle_cooldown = atoi(argv[7]);
-		char *scheduler = argv[8];
+		if (!is_digit(argv[i]))
+			return (1);
+		else if (atoi(argv[i]) < 0)
+			return (1);
+		i++;
 	}
-
-	printf("Usage: ./codexion number_of_coders time_to_burnout time_to_compile time_to_debug time_to_refactor number_of_compiles_required dongle_cooldown scheduler\n")
+	sim->number_coders = atoi(argv[1]);
+	if (sim->number_coders == 0)
+		return (1);
+	sim->burnout_time = atoi(argv[2]);
+	sim->compile_time = atoi(argv[3]);
+	sim->debug_time = atoi(argv[4]);
+	sim->refactor_time = atoi(argv[5]);
+	sim->refactor_time = atoi(argv[6]);
+	sim->refactor_time = atoi(argv[7]);
+	sim->scheduler = argv[8];
+	if (strcmp(sim->scheduler, "edf") && strcmp(sim->scheduler, "fifo"))
+		return (1);
+	return (0);
 }
-int	main(int argc, char *argv[])
-{
-	parse_arguments(argc, argv, sim);
 
-	return 1;
+int	main(int argc, char **argv)
+{
+	int				r;
+	t_simulation	sim;
+
+	if (argc != 9)
+	{
+		write(2, "Usage: ./codexion number_of_coders time_to_burnout", 51);
+		write(2, " time_to_compile time_to_debug", 31);
+		write(2, " time_to_refactor number_of_compiles_required", 46);
+		write(2, " dongle_cooldown scheduler\n", 28);
+		return (1);
+	}
+	r = parse_arguments(argv, &sim);
+	if (r > 0)
+		write(2, "error\n", 6);
+	return (1);
 }
--- codexion.h
+++ codexion.h
@@ -1,11 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   codexion.h                                        :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/04/16 18:18:52 by username         #+#    #+#              */
+/*   Updated: 2026/04/16 18:27:56 by username        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CODEXION_H
 # define CODEXION_H
 
 # include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
+# include <string.h>
 
-typedef struct{
-
-}   t_simulation;
+typedef struct s_simulation
+{
+	int		number_coders;
+	int		burnout_time;
+	char	*scheduler;
+	int		compile_time;
+	int		debug_time;
+	int		refactor_time;
+	int		number_compiles;
+	int		dongle_cooldown;
+}	t_simulation;
 
 #endif
