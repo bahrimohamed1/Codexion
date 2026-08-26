eeb25893fa2acc51d4e11b4cb5a3d75acf22c791
Mohamed Bahri <simo@Mohameds-MacBook-Air.local>
2026-08-26 22:00:02 +0100

    Added logger.c

--- logger.c
+++ logger.c
@@ -6,7 +6,7 @@
 /*   By: mbahri <mbahri@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 19:40:09 by mbahri            #+#    #+#             */
-/*   Updated: 2026/08/26 19:41:00 by mbahri           ###   ########.fr       */
+/*   Updated: 2026/08/26 21:59:45 by mbahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,33 @@
 
 void	log_state(t_coder *coder, t_state state)
 {
-    
+    t_simulation	*sim;
+	long			time;
+	sim = coder->simulation;
+	pthread_mutex_lock(&sim->state_mutex);
+	if (sim->stop && state != BURNED_OUT)
+	{
+		pthread_mutex_unlock(&sim->state_mutex);
+		return ;
+	}
+	pthread_mutex_lock(&sim->log_mutex);
+	pthread_mutex_unlock(&sim->state_mutex);
+	time = get_elapsed_time(sim->start_time);
+	printf("%ld %d %s\n", time, coder->id, state_message(state));
+	pthread_mutex_unlock(&sim->log_mutex);
 }
 
 char	*state_message(t_state state)
 {
-    
+    if (state == TAKE_DONGLE)
+		return ("has taken a dongle");
+	if (state == COMPILING)
+		return ("is compiling");
+	if (state == DEBUGGING)
+		return ("is debugging");
+	if (state == REFACTORING)
+		return ("is refactoring");
+	if (state == BURNED_OUT)
+		return ("burned out");
+	return (NULL);
 }
