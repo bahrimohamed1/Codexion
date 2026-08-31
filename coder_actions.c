c469f2e3919680c139498b19753260314d95eaa7
Mohamed Bahri <simo@Mohameds-MacBook-Air.local>
2026-08-31 04:58:27 +0100

    Added debug and refactor phases

--- coder_actions.c
+++ coder_actions.c
@@ -6,7 +6,7 @@
 /*   By: mbahri <mbahri@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 03:54:05 by mbahri            #+#    #+#             */
-/*   Updated: 2026/08/31 04:01:40 by mbahri           ###   ########.fr       */
+/*   Updated: 2026/08/31 04:48:57 by mbahri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	compile_phase(t_coder *coder)
 	t_simulation	*sim;
 
 	sim = coder->simulation;
+	if (simulation_stopped(sim))
+	{
+		release_dongles(coder);
+		return (0);
+	}
 	pthread_mutex_lock(&coder->state_mutex);
 	coder->last_compile_start = get_time_ms();
 	pthread_mutex_unlock(&coder->state_mutex);
@@ -28,5 +33,33 @@ int	compile_phase(t_coder *coder)
 	pthread_mutex_lock(&coder->state_mutex);
 	coder->compile_count++;
 	pthread_mutex_unlock(&coder->state_mutex);
+	return (1);
+}
+
+int	debug_phase(t_coder *coder)
+{
+	t_simulation	*sim;
+
+	sim = coder->simulation;
+	if (simulation_stopped(sim))
+		return (0);
+	log_state(coder, DEBUGGING);
+	sim_sleep(sim, sim->config.time_to_debug);
+	if (simulation_stopped(sim))
+		return (0);
+	return (1);
+}
+
+int	refactor_phase(t_coder *coder)
+{
+	t_simulation	*sim;
+
+	sim = coder->simulation;
+	if (simulation_stopped(sim))
+		return (0);
+	log_state(coder, REFACTORING);
+	sim_sleep(sim, sim->config.time_to_refactor);
+	if (simulation_stopped(sim))
+		return (0);
 	return (1);
 }
